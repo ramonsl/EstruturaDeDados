ff3761692b1b83c3c4fcca53816111703e3f7edb
ramonsl <ramonsl@gmail.com>
2016-08-16 17:00:23 -0300

    Aula 03
    Aula 03

---
+++ Aulas/03/src/ed1/ulbra/Main.java
@@ -0,0 +1,19 @@
+package ed1.ulbra;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int vetor[] = new int [5];// vetor de 5 elementos
+        System.out.println(vetor.length);  // mostra o tamanho do vetor
+        int matriz[][] = new int [5][4];// matrix de 5x5 elementos
+        System.out.println(matriz.length); // numero de linhas
+        System.out.println(matriz[0].length); // numero de colunas
+
+
+
+    }
+
+
+}
